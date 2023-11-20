e49cf5719ca92d85087ba301c67acbe6d3dd77b8
spacegalaxy2324 <148562155+spacegalaxy2324@users.noreply.github.com>
2023-11-20 15:48:22 +0100

    first commit

---
+++ lib/api/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const baseUrl = "https://api.themoviedb.org/3/";
+  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+  static const apiKey = "9f9ac8ae339240fb5d6d73120a4b2827";
+}
